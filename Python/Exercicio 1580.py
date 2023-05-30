e06ec22d306d5a300b6f237aa73f17b359752146
Davi Gouveia <davitgouveia@gmail.com>
2023-05-30 19:56:07 -0300

    Added exc1580

--- Python/Exercicio 1580.py
+++ Python/Exercicio 1580.py
@@ -4,9 +4,6 @@ def countAnagrams(word):
     char_frequency = {}
     
     def factorial(n):
-        # if n <= 1:
-        #     return 1
-        # return n * factorial(n - 1)
         fact = 1
         for i in range(1, n+1):
             fact = fact * i
